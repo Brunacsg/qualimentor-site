d57676159106a0a1eb30b825e78826477767ed5e
Brunacsg <bruna_cs27@hotmail.com>
2026-04-07 10:23:45 -0300

    deploy render

--- server.js
+++ server.js
@@ -331,6 +331,7 @@ app.get('/dashboard', auth, (req, res) => {
 // =========================
 // START SERVER
 // =========================
-app.listen(3000, () => {
-  console.log('🚀 Rodando em http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Rodando em http://localhost:${PORT}`);
 });
